865130b1961253a9e896f763013e7a19ba954397
KimYiHyun <chkuy123@naver.com>
2022-01-17 23:58:11 +0900

    add: 생년월일 캘린더 추가

--- app/src/main/java/com/example/kokoapplication/SignupActivity.kt
+++ app/src/main/java/com/example/kokoapplication/SignupActivity.kt
@@ -1,11 +1,37 @@
 package com.example.kokoapplication
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import java.util.*
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val startdate : EditText = findViewById(R.id.birth)
+
+        startdate.setInputType(InputType.TYPE_NULL);
+
+
+        startdate.setOnClickListener{
+            val cldr: Calendar = Calendar.getInstance()
+            val day: Int = cldr.get(Calendar.DAY_OF_MONTH)
+            val month: Int = cldr.get(Calendar.MONTH)
+            val year: Int = cldr.get(Calendar.YEAR)
+            // date picker dialog
+            var picker: DatePickerDialog
+
+            picker = DatePickerDialog(this@SignupActivity,
+                { view, year, monthOfYear, dayOfMonth -> startdate.setText(dayOfMonth.toString() + "/" + (monthOfYear + 1) + "/" + year) },
+                year,
+                month,
+                day
+            )
+            picker.show()
+        }
     }
 }
