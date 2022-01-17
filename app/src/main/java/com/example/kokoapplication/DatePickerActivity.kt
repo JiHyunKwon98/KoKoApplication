1dc60be21f42526c688f4c3a25a22998f0592ad9
권지현 <dial2825@naver.com>
2022-01-18 00:02:21 +0900

    modify : Rebase

--- app/src/main/java/com/example/kokoapplication/DatePickerActivity.kt
+++ app/src/main/java/com/example/kokoapplication/DatePickerActivity.kt
@@ -1,4 +1,5 @@
 package com.example.kokoapplication;
+
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle;
@@ -14,50 +15,58 @@ class DatePickerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_date_picker)
 
-        val picker : DatePicker = findViewById(R.id.datePicker1)
-        val btnGet : Button = findViewById(R.id.button1)
-        val tvw : TextView = findViewById(R.id.textView1)
+        val picker: DatePicker = findViewById(R.id.datePicker1)
+
 
         val minDate = getInstance()
         val maxDate = getInstance()
 
-        minDate.set(2020,1-1,1);
+        minDate.set(2020, 1 - 1, 1);
 
         picker.setMinDate(minDate.getTime().getTime());
 
-        maxDate.set(2024,1-1,1);
+        maxDate.set(2024, 1 - 1, 1);
         picker.setMaxDate(maxDate.getTimeInMillis());
+
+        /*
+        val tvw : TextView = findViewById(R.id.textView1)
+        val btnGet : Button = findViewById(R.id.button1)
 
         btnGet.setOnClickListener {
             tvw.setText("Selected Date: "+ picker.getDayOfMonth()+"/"+ (picker.getMonth() + 1)+"/"+picker.getYear());
+        }*/
+
+        val chagemode: ImageView = findViewById(R.id.chagemode)
+
+        chagemode.setOnClickListener {
+            val intent = Intent(this@DatePickerActivity, WeekActivity::class.java)
+            startActivity(intent)
         }
 
+        val gograp: ImageView = findViewById(R.id.gograp)
 
-        val gograp : ImageView = findViewById(R.id.gograp)
+        val golist: ImageView = findViewById(R.id.golist)
 
-        val golist : ImageView = findViewById(R.id.golist)
+        val gohome: ImageView = findViewById(R.id.gohome)
 
-        val gohome : ImageView = findViewById(R.id.gohome)
+        val gomy: ImageView = findViewById(R.id.gomy)
 
-        val gomy : ImageView = findViewById(R.id.gomy)
-
-        gograp.setOnClickListener{
+        gograp.setOnClickListener {
             val intent = Intent(this@DatePickerActivity, GraphActivity::class.java)
             startActivity(intent)
         }
 
-        golist.setOnClickListener{
+        golist.setOnClickListener {
             val intent = Intent(this@DatePickerActivity, ManageActivity::class.java)
             startActivity(intent)
         }
 
-        gohome.setOnClickListener{
+        gohome.setOnClickListener {
             val intent = Intent(this@DatePickerActivity, DatePickerActivity::class.java)
             startActivity(intent)
         }
 
-
-        gomy.setOnClickListener{
+        gomy.setOnClickListener {
             val intent = Intent(this@DatePickerActivity, MyActivity::class.java)
             startActivity(intent)
         }
