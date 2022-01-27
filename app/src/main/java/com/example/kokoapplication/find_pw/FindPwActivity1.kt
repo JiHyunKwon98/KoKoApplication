aace36ab43e34b75fad1002d953535b8240c9b0d
KimYiHyun <chkuy123@naver.com>
2022-01-27 14:55:32 +0900

    add : 화면전환 적용

--- app/src/main/java/com/example/kokoapplication/find_pw/FindPwActivity1.kt
+++ app/src/main/java/com/example/kokoapplication/find_pw/FindPwActivity1.kt
@@ -1,12 +1,19 @@
 package com.example.kokoapplication.find_pw
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.kokoapplication.R
 
 class FindPwActivity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_pw1)
+    }
+
+    fun clickPw1(view: View) {
+        var intent = Intent(this, FindPwActivity2::class.java)
+        startActivity(intent)
     }
 }
