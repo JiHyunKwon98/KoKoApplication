d9db3258f008ada297c51750082b5f7df201a10e
KimYiHyun <chkuy123@naver.com>
2022-01-27 17:26:49 +0900

    add : timepicker 적용

--- app/src/main/java/com/example/kokoapplication/SleepActivity.kt
+++ app/src/main/java/com/example/kokoapplication/SleepActivity.kt
@@ -1,11 +1,24 @@
 package com.example.kokoapplication
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TimePicker
 
 class SleepActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sleep)
+
+        hideTimeHeaderLayout(findViewById(R.id.sleep_timePicker))
+    }
+
+    private fun hideTimeHeaderLayout(picker: TimePicker) {
+        val id = Resources.getSystem().getIdentifier("time_header", "id", "android")
+        val timeLayout = picker.findViewById<View>(id)
+        if (timeLayout != null) {
+            timeLayout.visibility = View.GONE
+        }
     }
 }
