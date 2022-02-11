95de4712ca2706b98889ff1f32f2eb28d2801843
KimYiHyun <chkuy123@naver.com>
2022-02-11 23:40:28 +0900

    modify : 파일경로 변경

--- app/src/main/java/com/example/kokoapplication/sleep/SleepActivity.kt
+++ app/src/main/java/com/example/kokoapplication/sleep/SleepActivity.kt
@@ -1,10 +1,11 @@
-package com.example.kokoapplication
+package com.example.kokoapplication.sleep
 
 import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TimePicker
+import com.example.kokoapplication.R
 
 class SleepActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
